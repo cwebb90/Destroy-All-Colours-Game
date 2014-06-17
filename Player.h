8430ca5573ec9e9e00bcc7ab7d8f88e94002478b
Christopher Webb <Chris_Webb184@Hotmail.com>
2014-06-17 12:01:10 +0100

    Create Player.h
    Player class definitions

---
+++ Player.h
@@ -0,0 +1,29 @@
+/*Christopher Webb
+Project PRJ254
+Destroy All Colours Version 1
+Player.h*/
+
+#pragma once
+#include <iostream>
+#include <string>
+#include "Entity.h"
+#include "Level.h"
+
+using namespace std;
+
+class Player : public Entity
+{
+private:
+	int m_points;			//Player will need to attain certain amount of points to complete the level.
+	int ballwait;			//this variable will help slow the ball-dropping function, so the player does not drop balls too fast.
+	int movecycle;
+
+public:
+	Player(int xpos, int ypos, string name, string filename, Level* level, int points);
+	int getPoints();
+	void setPoints(int points);
+	void gainPoint();
+	void dropBall();
+	void playerLeft();
+	void playerRight();
+};
